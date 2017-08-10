d9445f5c2301bcf5c51120e7bb8c4d2415a73f06
HTNecro <HTNecro@gmail.com>
2017-08-10 19:39:57 +0800

    log4j2 pom

---
+++ demo-web/src/main/java/org/htnecro/demo/util/Log.kt
@@ -0,0 +1,11 @@
+package org.htnecro.demo.util
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ *
+ *
+ * @author Haitao.Wu (HTNecro@gmail.com)
+ */
+fun log(any: Any): Logger = LoggerFactory.getLogger(any.javaClass)
