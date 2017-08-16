ef673d78605aad3411ffaf6fe8182c5c84f74f36
HTNecro <HTNecro@gmail.com>
2017-08-16 20:58:03 +0800

    upgrade kotlin version

--- demo-web/src/main/java/org/htnecro/demo/util/Log.kt
+++ demo-web/src/main/java/org/htnecro/demo/util/Log.kt
@@ -4,8 +4,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 /**
- *
- *
  * @author Haitao.Wu (HTNecro@gmail.com)
  */
 fun log(any: Any): Logger = LoggerFactory.getLogger(any.javaClass)
