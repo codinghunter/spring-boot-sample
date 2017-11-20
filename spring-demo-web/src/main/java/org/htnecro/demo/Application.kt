81963bb18f2675e40a9ce9ff83441224090d0fbc
HTNecro <HTNecro@gmail.com>
2017-11-20 17:13:45 +0800

    rm enableTransaction annotation

--- spring-demo-web/src/main/java/org/htnecro/demo/Application.kt
+++ spring-demo-web/src/main/java/org/htnecro/demo/Application.kt
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import javax.annotation.Resource
 
 @EnableAsync
-@EnableTransactionManagement
 @SpringBootApplication
 class Application : WebMvcConfigurerAdapter() {
 
