8d12d41d39dc7e48fa3d72618c6188ab2dbf0580
HTNecro <HTNecro@gmail.com>
2018-09-14 14:13:46 +0800

    add spring security

---
+++ spring-demo-web/src/main/java/org/htnecro/demo/configuration/WebConfiguration.java
@@ -0,0 +1,57 @@
+package org.htnecro.demo.configuration;
+
+import org.htnecro.demo.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.inject.Inject;
+
+@EnableWebSecurity
+public class WebConfiguration {
+
+    @Configuration
+    public static class InterceptorConfiguration implements WebMvcConfigurer {
+
+        private final LoginInterceptor loginInterceptor;
+
+        @Inject
+        public InterceptorConfiguration(LoginInterceptor loginInterceptor) {
+            this.loginInterceptor = loginInterceptor;
+        }
+
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(loginInterceptor);
+        }
+    }
+
+    @Configuration
+    public static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest().hasRole("ADMIN")
+                    .and()
+                    .formLogin().and()
+                    .httpBasic();
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService() {
+            User.UserBuilder users = User.withDefaultPasswordEncoder();
+            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+            manager.createUser(users.username("admin").password("password").roles("ADMIN").build());
+            return manager;
+        }
+    }
+}
--- spring-demo-web/src/main/java/org/htnecro/demo/interceptor/LoginInterceptor.kt
+++ spring-demo-web/src/main/java/org/htnecro/demo/interceptor/LoginInterceptor.kt
@@ -1,8 +1,8 @@
 package org.htnecro.demo.interceptor
 
+import org.htnecro.demo.util.log
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
-import java.lang.Exception
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -10,4 +10,11 @@ import javax.servlet.http.HttpServletResponse
  * @author Haitao.Wu (HTNecro@gmail.com)
  */
 @Component
-class LoginInterceptor : HandlerInterceptorAdapter()
+class LoginInterceptor : HandlerInterceptorAdapter() {
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        log(this).info("Login interceptor")
+        return super.preHandle(request, response, handler)
+    }
+
+}
